96f2afae65bb3470900d157e775f8abb3b8cf57e
TasSlavinskiy <taras.slavinskiy@gmail.com>
2021-03-29 16:17:36 +0300

    New changeled

--- home-work08/script.js
+++ home-work08/script.js
@@ -1,18 +1,48 @@
 
-    // the first task
+        // the first task
 
-    console.log(upperCase('regexp'))
-    console.log(upperCase('RegExp'))
+        console.log(upperCase('regexp'))
+        console.log(upperCase('RegExp'))
 
-    function dis(word, regexp) {
-        return regexp.test(word); 
-    }
+        function upperCase(word) {
+            let t = "String's not starts with uppercase character";
+            let f = "String's starts with uppercase character";
+            let regexp = /^[A-Z]/;
+            return (regexp.test(word)) ?  f : t 
+        }
 
-    function upperCase(word) {
-        let t = "String's not starts with uppercase character";
-        let f = "String's starts with uppercase character";
+        // the second task
 
-        return (dis(word) === true) ?  t : f 
-    }
+        function checkEmail(email) {
+            let regexp = /\S+@\S+\.\S+/;
+            return regexp.test(email);
+        }
 
-     // the second task
+        console.log(checkEmail("Qmail2@gmail.com"));
+        console.log(checkEmail("Qmai l2+gmail.com"));
+
+        // the third task
+
+        $(".head").css("background-color", "green").find(".inner").css("font-size", "35px");
+
+        // the fourth task 
+
+        var checkbox = $(".checkbox");
+            checkbox.on("click", function() {
+                if($(".checkbox:checked").length == 3) {
+                    checkbox.attr("disabled", "true");
+                }
+            })
+
+        // the fifth task
+
+        function getChangeWordPosition(word) {
+            let regexp = /(\w+)\s(\w+)/;
+            return word.replace(regexp, "$2, $1");
+        } 
+
+        console.log(getChangeWordPosition("Java Script"))
+        
+        // the sixth task
+
+        
